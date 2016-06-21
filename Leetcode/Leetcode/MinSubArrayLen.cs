5614ffd986f114b14c3fb67fb1ff341dd9ffaeb3
wenjie xu <garreyxu@gmail.com>
2016-06-20 17:40:43 -0700

    MinSubArrayLen added

---
+++ Leetcode/Leetcode/MinSubArrayLen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class MinSubArrayLen
+    {
+        public int GetMinSubArrayLen(int s, int[] nums)
+        {
+            return 0;
+        }
+    }
+}
