5a43e6a9eef3386514ea4ead528e5f3e2609f1c7
Garrey Xu <garreyxu@gmail.com>
2018-01-24 19:24:04 -0800

    start lc 675

---
+++ Leetcode/Leetcode/CutOffTreesForGolfEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class CutOffTreesForGolfEvent
+    {
+        public CutOffTreesForGolfEvent() { }
+
+        public int CutOffTree(IList<IList<int>> forest)
+        {
+            int result = -1;
+
+            if (!CutAllTree(forest))
+                result = -1;
+
+            return result;
+        }
+
+        public bool CutAllTree(IList<IList<int>> forest)
+        {
+            foreach (var row in forest)
+            {
+                foreach (var column in row)
+                {
+                    if (column > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
