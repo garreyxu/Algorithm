bb12dd8d2b2c5e464bfa8d43fef4d44828941451
Garrey Xu <garreyxu@gmail.com>
2018-01-29 20:25:27 -0800

    Algorithm from HackerRank: MIssingNumbers

---
+++ Leetcode/Leetcode/MissingNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class MissingNumbers
+    {
+        public MissingNumbers(){}
+
+        public static int[] missingNumbers(int[] arr, int[] brr)
+        {
+            // Complete this function
+            int[] temp = new int[10001];
+            ArrayList result = new ArrayList();
+
+            if (arr.Length == brr.Length)
+                return result.ToArray(typeof(int)) as int[];
+
+            for (int i = 0; i <= 10000; i++)
+                temp[i] = 0;
+
+            foreach (var number in arr)
+            {
+                temp[number]--;
+            }
+
+            foreach (var number in brr)
+            {
+                temp[number]++;
+            }
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] > 0)
+                {
+                    while (temp[i] > 0)
+                    {
+
+                        result.Add(i);
+                        temp[i]--;
+                    }
+                }
+            }
+
+            return result.ToArray(typeof(int)) as int[];
+        }
+    }
+}
--- Leetcode/Leetcode/Program.cs
+++ Leetcode/Leetcode/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace Leetcode
 {
@@ -7,8 +9,26 @@ namespace Leetcode
     {
         public static void Main()
         {
-
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            int m = Convert.ToInt32(Console.ReadLine());
+            string[] brr_temp = ReadLine().Split(' ');
+            int[] brr = Array.ConvertAll(brr_temp, Int32.Parse);
+            int[] result = MissingNumbers.missingNumbers(arr, brr);
+            Console.WriteLine(String.Join(" ", result));
+            Console.ReadLine();
         }
-        
+
+        private static string ReadLine()
+        {
+            Stream inputStream = Console.OpenStandardInput(1000);
+            byte[] bytes = new byte[1000];
+            int outputLength = inputStream.Read(bytes, 0, 1000);
+            //Console.WriteLine(outputLength);
+            char[] chars = Encoding.UTF7.GetChars(bytes, 0, outputLength);
+            return new string(chars);
+        }
+
     }
 }
